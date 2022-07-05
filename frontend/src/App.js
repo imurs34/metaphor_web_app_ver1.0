17a62b7e46d54c4c93412ce3eea708f708d8d943
riisatoro <riisa.toro@gmail.com>
2022-07-05 20:13:03 +0300

    Add context menue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,26 @@ import MDEditor from '@uiw/react-md-editor';
 import rehypeSanitize from "rehype-sanitize";
 
 function App() {
-  const [text, setText] = useState();
+  const [text, setText] = useState("Some body once told me the world is gonna roll me...");
+  const [selectedText, setSelectedText] = useState("");
+  const [contextMenuPosition, setContextMenuPosition] = useState({x: 0, y: 0});
 
-  const openContextMenu = () => {}
+  const openContextMenu = (event) => {
+    const text = window.getSelection()?.toString();
+    if (!text) {
+      setSelectedText('');
+      return; 
+    };
+    setContextMenuPosition({x: event.clientX, y: event.clientY + 10})
+    setSelectedText(text);
+  };
+
+  const ContextMenu = ({x, y}) => (
+    <div className="position-absolute bg-" style={{top: `${y}px`, left: `${x}px`}}>
+      <button>Main metaphor</button>
+      <button>Sub-metaphor</button>
+    </div>
+  );
 
   return (
     <div className="container pt-5">
@@ -21,6 +38,7 @@ function App() {
             preview="edit"
             onMouseUp={openContextMenu}
           />
+          {selectedText && <ContextMenu {...contextMenuPosition} />}
         </div>
         <div className="col-6">
           <div className="row">
