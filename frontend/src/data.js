d1b8132d2cd7c2a817890124ff69b5b27bf24ffd
riisatoro <riisa.toro@gmail.com>
2022-07-07 15:53:16 +0300

    Update text

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -41,7 +41,29 @@ export const dna = {
         rope: "DNA is like a rope in that it is made up of many smaller strands twisted together.",
         string: "A DNA molecule is like a string because it is long and thin.",
       },
-      explanation: {},
+      explanation: {
+        helix: {
+          raw: "DNA is the foundation of our lives. The double helix is the structure that DNA takes. The base pairs are the bricks that make up the DNA. The nucleotides are the mortar that holds the bricks together. The chromosomes are the walls of the DNA. The genes are the windows of the DNA, letting in light and information.",
+          writing:
+            "DNA is the foundation of our lives. It is the instruction manual for our bodies, telling us how to grow, develop, and function. The double helix is the structure that DNA takes. The base pairs are the bricks that make up the DNA. The nucleotides are the mortar that holds the bricks together. The chromosomes are the walls of the DNA. The genes are the windows of the DNA, letting in light and information.\n\nEach of us has a unique DNA blueprint. This blueprint is like a set of instructions that tells our cells what to do. DNA is made up of two long strands of molecules called nucleotides. These strands are coiled around each other to form a double helix. The nucleotides are held together by weak bonds called hydrogen bonds.\n\nThe double helix looks like a twisted ladder. The rungs of the ladder are made up of pairs of nucleotides. There are four types of nucleotides in DNA: adenine (A), thymine (T), cytosine (C), and guanine (G). The order of the nucleotides in DNA determines the information it contains.\n\nDNA is found in the nucleus of cells, and it is organized into chromosomes. Humans have 23 pairs of chromosomes, for a total of 46. One set of chromosomes comes from the mother, and the other set comes from the father.",
+          charts: [
+            ["DNA", "foundation"],
+            ["helix", "structure"],
+            ["pairs", "bricks"],
+            ["nucleotides", "mortar"],
+            ["chromosomes", "walls of the DNA"],
+            ["genes", "windows"],
+          ],
+          chartText: [
+            "of our lives",
+            "that DNA takes",
+            "make up the DNA",
+            "holds the bricks together",
+            "N/A",
+            "letting in light and information",
+          ],
+        },
+      },
     },
     {
       cluster_label: ["maze", "labyrinth", "honeycomb", "staircase", "path"],
@@ -141,26 +163,113 @@ export const dna = {
           raw: "DNA is the recipe. The base pairs are the ingredients, and the sequence of base pairs is the order in which they must be added. The DNA polymerase is the chef, adding new base pairs to the DNA strand. The DNA helicase is the mixer, separating the DNA strands so that the polymerase can add new base pairs. The DNA ligase is the glue, sealing the DNA strand back together.",
           writing:
             'DNA is the recipe for life. It contains the instructions for building proteins, which are the building blocks of all living things. The DNA molecule is made up of two strands of sugar and phosphate molecules, which are held together by pairs of nitrogen-containing molecules called bases.\n\nThe sequence of bases in DNA determines the sequence of amino acids in proteins. Amino acids are the building blocks of proteins, and they are arranged in a particular order to form specific proteins.\n\nDNA is like a recipe because it contains the instructions for building proteins. The bases are like the ingredients in a recipe, and the sequence of bases is like the order in which they must be added. The DNA polymerase is like the chef, adding new base pairs to the DNA strand. The DNA helicase is like the mixer, separating the DNA strands so that the polymerase can add new base pairs. The DNA ligase is like the glue, sealing the DNA strand back together."',
+          charts: [
+            ["DNA", "recipe"],
+            ["base pairs", "ingredients"],
+            ["DNA polymerase", "chef"],
+            ["DNA helicase", "mixer"],
+            ["DNA ligase", "glue"],
+          ],
+          chartText: [
+            "sequence of base pairs is the order in which they should be added",
+            "adding new base pairs to the DNA strand",
+            "separating the DNA strands so that the polymerase",
+            "can add new base pairs",
+            "sealing the DNA strand back together",
+          ],
         },
         book: {
           raw: "DNA is the book. The nucleotides are the letters, spelling out the genetic code. The sugar-phosphate backbone is the spine of the book, holding everything together. The base pairs are the pages, with the information written on them. The double helix is the structure of the book, with the two strands winding around each other.",
           writing:
             "DNA is the molecule that contains the genetic instructions for the development and function of living organisms. DNA is a double-stranded molecule held together by weak bonds between the bases, or nucleotides, on each strand. The bases are arranged in a specific order, and the order of the bases determines the function of the DNA.\n\nThe DNA molecule is like a book. The nucleotides are the letters, spelling out the genetic code. The sugar-phosphate backbone is the spine of the book, holding everything together. The base pairs are the pages, with the information written on them. The double helix is the structure of the book, with the two strands winding around each other.\n\nThe DNA molecule is made up of two strands of nucleotides, which are held together by weak bonds between the bases. The order of the bases on each strand determines the function of the DNA. The two strands of DNA are coiled around each other to form a double helix.",
+          charts: [
+            ["DNA", "book"],
+            ["nucleotides", "letters"],
+            ["sugar-phosphate backbone", "spine of the book"],
+            ["base pairs", "pages"],
+            ["double helix", "structure of the book"],
+          ],
+          chartText: [
+            "",
+            "spelling out the genetic code",
+            "holding everything together",
+            "with the information written on them",
+            "with the two strands winding around each other",
+          ],
         },
         staircase: {
           raw: "DNA is the staircase. The double helix is the structure of the staircase, with the rungs being the base pairs. The nucleotides are the steps, going up and down the staircase. The sugar-phosphate backbone is the handrail, keeping everything in place.",
           writing:
             "The double helix structure of DNA is held together by weak hydrogen bonds between the bases. The bases are arranged in a specific order, with adenine (A) always paired with thymine (T), and cytosine (C) always paired with guanine (G). This order is what encodes the information in DNA. The sequence of bases determines the order of amino acids in proteins, which in turn determine the structure and function of the protein.\n\nDNA is like a staircase, with the base pairs being the rungs and the sugar-phosphate backbone being the handrail. The nucleotides are the steps, going up and down the staircase. This metaphor can help you understand the structure of DNA and how the sequence of bases encodes information.",
+          charts: [
+            ["DNA", "staircase"],
+            ["double helix", "structure of the staircase"],
+            ["nucleotides", "steps"],
+            ["sugar-phosphate backbone", "handrail"],
+          ],
+          chartText: [
+            "",
+            "with the rungs being the base pairs",
+            "going up and down the staircase",
+            "keeping everything in place",
+          ],
         },
         map: {
           raw: "DNA is the map of a city. The base pairs are the streets, laid out in a specific order. The genes are the landmarks, telling us where everything is. The chromosomes are the districts, grouping related genes together. The DNA molecule is the city itself, containing all the information needed to build and maintain a living organism.",
           writing:
             "DNA is the map of a city. The base pairs are the streets, laid out in a specific order. The genes are the landmarks, telling us where everything is. The chromosomes are the districts, grouping related genes together. The DNA molecule is the city itself, containing all the information needed to build and maintain a living organism.\n\nEach cell in our bodies contains a complete set of instructions for how that cell should look and function. This instruction book is stored in our DNA, in the form of a code made up of four chemical letters: A, T, C, and G. The order of these letters, known as the DNA sequence, determines the instructions for building each cell.\n\nDNA is like a long ladder, with the rungs made up of these chemical letters. The sides of the ladder are made of sugar and phosphate molecules. The rungs are held together by weak bonds between the letters.\n\nThe DNA molecule is very long and thin, about two millionths of a millimeter wide. If you were to stretch out the DNA in all the cells in your body, it would be about two meters long!\n\nThe DNA code is read in units of three letters, called codons. Each codon specifies a particular amino acid, which are the building blocks of proteins. There are 64 possible codons, which can be divided into 20 different amino acids.\n\nThe DNA sequence is read from the beginning of the molecule to the end, in a linear fashion. The sequence is then used to build the proteins needed by the cell. Proteins are the machines that carry out the cell's functions.",
+          charts: [
+            ["DNA", "map of a city"],
+            ["base pairs", "streets"],
+            ["genes", "landmarks"],
+            ["chromosomes", "districts"],
+            ["DNA molecule", "city itself"],
+          ],
+          chartText: [
+            "laid out in a specific order",
+            "telling us where everything is",
+            "grouping related genes together",
+            "containing all the information needed to build and maintain a living organism",
+          ],
         },
         building: {
           raw: "DNA is a set of instructions for building a house. The DNA molecule is the blueprint, containing all the information needed to build the house. The nucleotides are the building blocks, the basic units that make up the DNA molecule. The chromosomes are the construction workers, assembling the nucleotides into DNA molecules. The genes are the architects, designing the DNA molecule.",
           writing:
-            'DNA is a set of instructions for building a house. The DNA molecule is the blueprint, containing all the information needed to build the house. The nucleotides are the building blocks, the basic units that make up the DNA molecule. The chromosomes are the construction workers, assembling the nucleotides into DNA molecules. The genes are the architects, designing the DNA molecule.\n\nThe DNA molecule is a long, double-stranded chain of nucleotides. The nucleotides are joined together by covalent bonds between the phosphate group of one nucleotide and the sugar group of the next nucleotide. The sequence of nucleotides in a DNA molecule determines the sequence of amino acids in a protein.\n\nThe DNA molecule is divided into two parts: the coding region and the non-coding region. The coding region is the part of the DNA molecule that contains the instructions for making a protein. The non-coding region is the part of the DNA molecule that does not contain the instructions for making a protein.\n\nThe coding region is divided into genes. Each gene contains the instructions for making a specific protein. The proteins made by the genes are responsible for the structure and function of the cells."',
+            "DNA is a set of instructions for building a house. The DNA molecule is the blueprint, containing all the information needed to build the house. The nucleotides are the building blocks, the basic units that make up the DNA molecule. The chromosomes are the construction workers, assembling the nucleotides into DNA molecules. The genes are the architects, designing the DNA molecule.\n\nThe DNA molecule is a long, double-stranded chain of nucleotides. The nucleotides are joined together by covalent bonds between the phosphate group of one nucleotide and the sugar group of the next nucleotide. The sequence of nucleotides in a DNA molecule determines the sequence of amino acids in a protein.\n\nThe DNA molecule is divided into two parts: the coding region and the non-coding region. The coding region is the part of the DNA molecule that contains the instructions for making a protein. The non-coding region is the part of the DNA molecule that does not contain the instructions for making a protein.\n\nThe coding region is divided into genes. Each gene contains the instructions for making a specific protein. The proteins made by the genes are responsible for the structure and function of the cells.",
+          charts: [
+            ["DNA", "set of instructions"],
+            ["DNA molecule", "blueprint"],
+            ["nucleotides", "building blocks"],
+            ["chromosomes", "construction workers"],
+            ["genes", "architects"],
+          ],
+          chartText: [
+            "for building a house",
+            "containing all the information needed to build the house",
+            "the basic units that make up the DNA molecule",
+            "assembling the nucleotides into DNA molecules",
+            "designing the DNA molecule",
+          ],
+        },
+        program: {
+          raw: "DNA is a set of instructions for assembling a computer. The DNA strands are the wires, carrying information from one gene to another. The genes are the computer chips, processing that information. The proteins are the transistors, controlling the flow of information. The cell is the computer, and the body is the network that it is connected to.",
+          writing:
+            "DNA is the set of instructions for assembling a computer. The DNA strands are the wires, carrying information from one gene to another. The genes are the computer chips, processing that information. The proteins are the transistors, controlling the flow of information. The cell is the computer, and the body is the network that it is connected to.\n\nIn order for a cell to function, all of its parts must work together. The DNA is like a set of instructions that tells the cell what to do. It is made up of two long strands of molecules called nucleotides. These strands are coiled around each other to form a double helix. The DNA strands are held together by weak bonds between the nucleotides.\n\nThe DNA molecule is very long and thin. It is so long that if it were stretched out, it would be about two meters long. The DNA molecule is so thin that it would take about 10,000 of them to equal the thickness of a human hair.\n\nThe DNA molecule is made up of four types of nucleotides: Adenine (A), Cytosine (C), Guanine (G), and Thymine (T). These four nucleotides are arranged in pairs along the DNA strands. The order of the nucleotides determines the function of the DNA. For example, the sequence ATGCAT tells the cell to make the protein that is responsible for the cell’s structure.\n\nThe DNA molecule is like a set of instructions for assembling a computer. The DNA strands are the wires, carrying information from one gene to another. The genes are the computer chips, processing that information. The proteins are the transistors, controlling the flow of information. The cell is the computer, and the body is the network that it is connected to.",
+          charts: [
+            ["DNA", "a set of instructions"],
+            ["DNA strands", "wires"],
+            ["genes", "computer chips"][("proteins", "transistors")],
+            ["cell", "computer"],
+            ["body", "network"],
+          ],
+          chartText: [
+            "for assembling a computer",
+            "carrying information from one gene to another",
+            "processing that information",
+            "controlling the flow of information",
+            "N/A",
+            "it is connected to",
+          ],
         },
       },
     },
@@ -169,9 +278,11 @@ export const dna = {
       vehicle: ["gene", "molecule", "cell", "chromosome", "nucleus"],
       meaning: {
         gene: "DNA is the code for a gene, in the same way that a recipe is the code for a cake.",
-        molecule: "A DNA molecule is like a recipe for a cake. It contains all the instructions for how to make the cake, including the ingredients, the measurements, and the order in which to add them.",
+        molecule:
+          "A DNA molecule is like a recipe for a cake. It contains all the instructions for how to make the cake, including the ingredients, the measurements, and the order in which to add them.",
         cell: "A cell is like a building, and DNA is like the blueprint for that building.",
-        chromosome: "Just as DNA is the basic genetic material in humans and other organisms, chromosomes are the structures that DNA is packaged into.",
+        chromosome:
+          "Just as DNA is the basic genetic material in humans and other organisms, chromosomes are the structures that DNA is packaged into.",
         nucleus: "",
       },
       explanation: {},
